20d35349712f20d70b68ce47d3bf4bcd55ddb797
Vgonjor <vgj1993@gmail.com>
2019-11-20 13:40:28 +0100

    Category replaced

--- Script_YT.py
+++ Script_YT.py
@@ -4,4 +4,50 @@ Created on Wed Nov 20 10:22:49 2019
 
 @author: Victor Gonzalez Jornet
 """
+#Change directory
+import os
+os.chdir("C:\\Users\poni6\Desktop\Data Analysis\YouTube")
+os.getcwd()
+
+
+#Import libraries
+import pandas as pd
+import json
+import numpy as np
+
+#Get data
+US_df= pd.read_csv("Data\\USvideos.csv")
+
+with open('Data/US_category_id.json', 'r') as myfile:
+    US_id=myfile.read()
+US_id_p = json.loads(US_id)
+
+#Extracting  id and title from the json
+for row in US_id_p['items']:
+        print(row['id'] + " : " + row["snippet"]["title"])
+
+def getList_id(dict): 
+    list = [] 
+    for row in dict["items"]: 
+        list.append(row["id"]) 
+          
+    return list
+def getList_title(dict): 
+    list = [] 
+    for row in dict["items"]: 
+        list.append(row["snippet"]["title"]) 
+          
+    return list
+category_id = getList_id(US_id_p)
+category_id = list(map(int, category_id))
+category_title = getList_title(US_id_p)
+print(category_id)
+print(category_title)
+category = dict(zip(category_id, category_title))
+print(category)
+
+
+#Replace category id in df for title
+US_df["category_id"]
+US_df['category_id'].map(category).fillna(US_df['category_id'])
 
