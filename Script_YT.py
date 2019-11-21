b7302ac9c349a88b598213b79f93471378309a3e
Vgonjor <vgj1993@gmail.com>
2019-11-21 10:05:01 +0100

    First test model

--- Script_YT.py
+++ Script_YT.py
@@ -14,6 +14,9 @@ os.getcwd()
 import pandas as pd
 import json
 import numpy as np
+import pandas_profiling as pp
+
+
 
 #Get data
 US_df= pd.read_csv("Data\\USvideos.csv")
@@ -49,5 +52,31 @@ print(category)
 
 #Replace category id in df for title
 US_df["category_id"]
-US_df['category_id'].map(category).fillna(US_df['category_id'])
+US_df["category_id"]=US_df['category_id'].map(category).fillna(US_df['category_id'])
 
+#Explore data
+pp.ProfileReport(US_df) #Run this only on Jupyter
+
+#Changing data types of date variables
+US_df["publish_time"]=pd.to_datetime(US_df["publish_time"])
+US_df["trending_date"]=pd.to_datetime(US_df["trending_date"], format="%y.%d.%m")
+US_df["trending_date"]
+
+
+#Checking for duplicated rows
+duplicateRowsDF = US_df[US_df.duplicated()]
+print(duplicateRowsDF)
+US_df=US_df.drop_duplicates()
+
+US_df['category_id'].value_counts().plot("bar")
+US_df['channel_title'].value_counts().plot("bar")
+
+
+#Modeling
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(US_df.drop(["tags", "channel_title", "title", "category_id", "video_id", "thumbnail_link", "description", "trending_date", "publish_time"], axis = 1), US_df["category_id"], test_size=0.3)
+
+from sklearn.ensemble import RandomForestClassifier
+random_forest_classifier = RandomForestClassifier()
+random_forest_classifier.fit(X_train,y_train)
+y_pred_rfc = random_forest_classifier.predict(X_test)
