bd1ceecbecb21adff7d71c5b0e358f5a919b79aa
Vgonjor <vgj1993@gmail.com>
2019-11-26 13:23:41 +0100

    Title SA

--- Script_YT.py
+++ Script_YT.py
@@ -15,7 +15,8 @@ import pandas as pd
 import json
 import numpy as np
 import pandas_profiling as pp
-
+import pytz
+import datetime
 
 
 #Get data
@@ -54,17 +55,37 @@ print(category)
 US_df["category_id"]
 US_df["category_id"]=US_df['category_id'].map(category).fillna(US_df['category_id'])
 
+
+
+
+#Changing data types of date variables and adding new vars
+US_df["publish_time"]=pd.to_datetime(US_df["publish_time"])
+US_df["trending_date"]=pd.to_datetime(US_df["trending_date"], format="%y.%d.%m")
+US_df["like_dislike_ratio"]=US_df["likes"]/(US_df["likes"] + US_df["dislikes"])
+US_df["comments_per_view"]=US_df["comment_count"]/US_df["views"]
+US_df["likes_dislikes_per_view"]=(US_df["likes"] + US_df["dislikes"])/US_df["views"]
+US_df["likes_per_view"]=US_df["likes"]/US_df["views"]
+US_df["dislikes_per_view"]=US_df["dislikes"]/US_df["views"]
+
+US_df["publish_time"] = US_df["publish_time"].dt.tz_convert(None)
+US_df['publish_to_trending'] = US_df['trending_date']-US_df['publish_time']
+US_df['publish_to_trending'].plot
 #Explore data
 pp.ProfileReport(US_df) #Run this only on Jupyter
 
 US_df["category_id"].value_counts().plot("pie")
 
+import seaborn as sns
+sns.distplot(np.log(US_df['likes']+1))
+sns.distplot(np.log(US_df['dislikes']+1))
+sns.distplot(np.log(US_df['comment_count']+1))
+sns.distplot(np.log(US_df['views']+1))
 
-#Changing data types of date variables
-US_df["publish_time"]=pd.to_datetime(US_df["publish_time"])
-US_df["trending_date"]=pd.to_datetime(US_df["trending_date"], format="%y.%d.%m")
-US_df["like_dislike_ratio"]=US_df["likes"]/US_df["dislikes"]
-
+sns.distplot(np.log(US_df['like_dislike_ratio']+1))
+sns.distplot(np.log(US_df['comments_per_view']+1))
+sns.distplot(np.log(US_df['likes_dislikes_per_view']+1))
+sns.distplot(np.log(US_df['likes_per_view']+1))
+sns.distplot(np.log(US_df['dislikes_per_view']+1))
 
 #Checking for duplicated rows
 duplicateRowsDF = US_df[US_df.duplicated()]
@@ -117,3 +138,53 @@ print(z)
 print(np.where(z > 20))
 
 
+print(pd.Timedelta(US_df["trending_date"] - US_df["publish_time"]).days)
+
+
+
+#Sentiment Analysis
+#Title
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyser = SentimentIntensityAnalyzer()
+
+vader = []
+for title in US_df.title:
+    vader.append(analyser.polarity_scores(title))
+
+textblob = []
+
+for title in US_df.title:
+    blob = TextBlob(title)     
+    textblob.append(blob.sentiment)
+    
+textblob[0:5]
+vader[0:5]
+
+Names = ['title_polarity', 'title_subjectivity']
+df = pd.DataFrame.from_records(textblob, columns=Names)
+
+US_df['title_polarity'] = df.title_polarity
+US_df['title_subjectivity'] = df.title_subjectivity
+
+US_df.head()
+
+import matplotlib.pyplot as plt
+plot_data = US_df[US_df.title_polarity != 0]
+
+US_df.plot(kind = "scatter", x = "title_polarity", y = "views")
+
+fig, ax = plt.subplots(figsize=(8, 6))
+
+# Plot histogram of the polarity values
+plot_data.title_polarity.hist(bins=[-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3,
+                                        -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 
+                                        0.8, 0.9, 1],
+             ax=ax,
+             color="purple")
+
+plt.title("Polarity UPR tweet")
+
+plt.show()
+#Description
